fc8b9e56b33e5480e3f71f194f44a4d2e2d3c7d9
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2025-04-17 11:04:07 -0600

    no darkmode

--- src/components/Treeditems.tsx
+++ src/components/Treeditems.tsx
@@ -34,17 +34,17 @@ export function Treeditems() {
     >
       {cards.map((card, index) => (
         <CardContainer key={index} className="inter-var">
-          <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:border-white/[0.2] border-black/[0.1] w-full h-auto rounded-xl p-6 border">
+          <CardBody className="bg-gray-50 relative group/card  border-black/[0.1] w-full h-auto rounded-xl p-6 border">
             <CardItem
               translateZ={50}
-              className="text-xl font-bold text-neutral-600 dark:text-white"
+              className="text-xl font-bold text-neutral-600 "
             >
               {card.title}
             </CardItem>
             <CardItem
               as="p"
               translateZ={60}
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              className="text-neutral-500 text-sm max-w-sm mt-2 "
             >
               {card.description}
             </CardItem>
@@ -53,7 +53,7 @@ export function Treeditems() {
                 src={card.image}
                 height={1000}
                 width={1000}
-                className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                className=" h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
                 alt="thumbnail"
               />
             </CardItem>
@@ -63,7 +63,7 @@ export function Treeditems() {
                 as={Link}
                 href={card.link}
                 target="_blank"
-                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                className="px-4 py-2 rounded-xl bg-black text-white"
               >
                 See More
               </CardItem>
